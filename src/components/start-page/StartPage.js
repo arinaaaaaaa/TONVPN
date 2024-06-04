b318b322ec2a0cd4db4bec7ee7667679f46e58f5
akrumm <akrumm@k-tech.ru>
2024-06-04 13:56:15 +0300

    create simple start page

---
+++ src/components/start-page/StartPage.js
@@ -0,0 +1,16 @@
+import "./StartPage.scss"
+export default function StartPage() {
+    function sendWSPaymentRequest() {
+
+    }
+    return (
+        <div className="container">
+            <div className="launch-button">
+                <p>Для доступа к VPN<br/>нажмите на кнопку снизу</p>
+                <button onClick={}>
+                    <img src="/turn-on.png" alt="turn on icon"/>
+                </button>
+            </div>
+        </div>
+    )
+}
